1dc45fd6e411497fb0fb5c54bf7d9d11b46bb437
Scarlett <3030695074@qq.com>
2023-12-29 13:43:12 +0000

    update src/main/java/com/oracle/controller/Test.java.
    Signed-off-by: Scarlett <3030695074@qq.com>

--- src/main/java/com/oracle/controller/Test.java
+++ src/main/java/com/oracle/controller/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("第一次测试");
-
+        System.out.println("第1次测试");
         System.out.println("第二次测试");
 
         System.out.println("第三次测试");
